d8430b7b9367cc8c2cdda83092c06fb009547137
Luis Capiral <ihategmpls@gmail.com>
2026-02-15 22:35:32 +0800

    feat: added title, buttons, and css

--- algoVisualizer/src/App.tsx
+++ algoVisualizer/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import sortingVisualizer from './algo_visualizer/algo_visualizer';
+import SortingVisualizerLogic from './sortingVisualizer/SortingVisualizer';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      //this is the main component of the app, it will render the sorting visualizer component
-      <sortingVisualizer></sortingVisualizer>
+      <SortingVisualizerLogic />
     </div>
   )
 }
--- algoVisualizer/src/sortingVisualizer/sortingVisualizer.tsx
+++ algoVisualizer/src/sortingVisualizer/sortingVisualizer.tsx
@@ -1,15 +1,31 @@
-import React from "react";
-import "./sortingVisualizer.css";
+// TO STUDY import React, { useState } from "react";
+import "./SortingVisualizer.css";
 
-const sortingVisualizer: React.FC = () => {
-  // Your sorting visualizer logic will go here
+const SortingVisualizerLogic: React.FC = () => {
+  // Visualizer Logic is here 
+  // TO STUDY const [sliderValue, setSliderValue] = useState(50);
   
   return (
-    <div className="sortingVisualizer">
-      {/* Your visualization UI will go here */}
+    <div className="SortingVisualizer">
+      {/* Visual UI is here*/}
       <h1>Sorting Visualizer</h1>
+        <button className='btn random'>randomize</button>
+        <button className='btn play'>play</button>
+
+        {/* slider component TO STUDY*/}
+        {/* <div className="slider-container">
+          <label>Array Size: {sliderValue}</label>
+          <input
+            type="range"
+            min={10}
+            max={100}
+            value={sliderValue}
+            onChange={(e) => setSliderValue(Number(e.target.value))}
+          />
+        </div> */}
+        
     </div>
   );
 };
 
-export default sortingVisualizer;
+export default SortingVisualizerLogic;
